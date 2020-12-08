8131f6696be15d889de890ee2c2bcfbe042cd398
ozandabb <osanda.gamage.79@gmail.com>
2020-12-08 10:02:59 +0530

    files added

---
+++ src/Views/SignIn/Login.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class basicInfor extends React.Component {
+
+    render() {
+        return (
+            <div style={{justifyContent:"center"}}>
+                <h1>This is the login page</h1>
+            </div>
+        );
+    }
+}
+
+export default basicInfor;
