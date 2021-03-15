ee8160d921e3532cf170d1628b5d91c22c8210d7
ozandabb <osanda.gamage.79@gmail.com>
2021-03-15 21:53:23 +0530

    Staff Registration, Admin panel, Validations created

--- src/App.js
+++ src/App.js
@@ -4,8 +4,13 @@ import RootRouter from "./RootRouter";
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 import {store, persistor} from './Redux/Store/Store';
-
+import './custom.scss'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import "../src/Asserts/commoncss/alerts.css";
+
+toast.configure(); 
 
 function App() {
   return(
